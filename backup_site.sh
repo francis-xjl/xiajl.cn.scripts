3f305515979cdb864505bcb9b8e57d811eb23c1b
francis <francis.xjl@qq.com>
2018-05-16 22:49:05 +0800

    备份脚本

---
+++ backup_site.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+backdate=`date +%Y%m%d%H%M%S`
+backpath=/backup/xiajl.cn/
+backdb=(solo)
+
+#mkdir directory
+if [ ! -d $backpath ]; then
+    mkdir -p $backpath
+fi
+
+for db in ${backdb[@]}
+do
+  backfile=$backpath"/"$db"."$backdate".sql"
+  rm -rf $backfile".gz"
+  mysqldump  -hlocalhost -uroot -P12591 -p10GsKranPNS@FNOins-m   --skip-lock-tables   --default-character-set=utf8  $db>$backfile
+  gzip $backfile
+  # 复制最新数据库文件，以便备份
+  rm -rf $backpath/$db.latest.gz
+  cp $backfile".gz" $backpath/$db.latest.gz
+done
+
+# 备份图片等上传的资源
+rm -rf $backpath"xiajl.cn.tar.gz"
+cd $backpath
+tar -czf xiajl.cn.tar.gz $backpath
+
+#clear 30 days ago
+find $backpath -mtime +30 -exec rm -rf {} \;
---
+++ download_backup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: francis
+@contact: francis.xjl@qq.com
+@file: download_backup.py
+@time: 2018/5/15 9:58
+@desc:
+"""
+
+import requests
+import time
+
+BACK_UP_ROOT = "F:/xiajl.cn_backups/"
+BACK_UP_SITE = "http://[username]:[password]@backup.xiajl.cn/"
+
+def backup_db():
+    """
+    备份数据库文件
+    :return:
+    """
+    filename = BACK_UP_ROOT + "db_" + str(time.strftime("%Y%m%d%H%M%S")) + ".gz"
+    r = requests.get(BACK_UP_SITE + '/solo.latest.gz', stream=True)
+    with open(filename, 'wb') as fd:
+        for chunk in r.iter_content():
+            fd.write(chunk)
+    pass
+
+
+def backup_file():
+    """
+    备份图片文件
+    :return:
+    """
+    filename = BACK_UP_ROOT + "file_" + str(time.strftime("%Y%m%d%H%M%S")) + ".gz"
+    r = requests.get(BACK_UP_SITE + '/solo.latest.gz', stream=True)
+    with open(filename, 'wb') as fd:
+        for chunk in r.iter_content():
+            fd.write(chunk)
+    pass
+
+if __name__ == "__main__":
+    backup_db()
+    backup_file()
